a3b91e0b77c4bb028112a6673c1a28db01c2fc0f
zzz711 <zzz8910@gmail.com>
2015-12-07 11:46:31 -0500

    added parallel methods

--- lineDetect.cu
+++ lineDetect.cu
@@ -2,16 +2,29 @@
 #include <stdio.h>
 
 __global__  whiteDect(const uchar4* const rgbaImage, unsigned char* const newImage,  int numRows, int numCols){
-	int threadsPerBlock = blockDim.x * blockDim.y;
-	int blockId = blockIdx.y + (blockIdx.x * gridDim.y);
-	int threadId = threadIdx.y + (threadIdx.x * blockDim.y);
+	int col = threadIdx.x;
+	int row = blockIdx.x;
+	int xID = col + row * numRows;
+	uchar4 linedImage = rgbaImage[xID];	
 
-	int offset = (blockId * threadsPerBlock) + threadId;;
-
-	if(rgbaImage[offset].x == 255 && rgbaImage[offset].y == 255 && rgbaImage[offset].z == 255){//if  pixel is fully white. what if they're not fully white?
-		//do something
+	if(rgbaImage[xID].x != 255 && rgbaImage[xID].y != 255 && rgbaImage[xID].z != 255){//if  pixel isn't fully white
+		newImage[xID] = linedImage.x * 0 + linedImage.y * 0 + linedImage.z*0; //make pixel black
 	}
-	else{
-		newImag = 
+
+}
+
+__global__  whiteToRed(const uchar4* const rgbaImage, unsigned char* const newImage,  int numRows, int numCols){
+	int col = threadIdx.x;
+	int row = blockIdx.x;
+	int xID = col + row * numRows;
+	uchar4 linedImage = rgbaImage[xID];	
+
+	if( rgaImage[xID].x == 255 && rgaImage[xID].y == 255 && rgaImage[xID].z == 255){
+		newImage[xID] = linedImage.x * 1 + linedImage.y * 0 + linedImage.z * 0; //white to red
+	}
+
+}
+
+void lineDetect(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage, unsigned char* const d_greyImage, size_t numRows, size_t numCols) {
 
 
